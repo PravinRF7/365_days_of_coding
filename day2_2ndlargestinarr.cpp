9784e5f4fcc7418f80cfc019e0e1baf174750cc0
Pravin <rfpravin@gmail.com>
2024-02-02 23:35:53 +0530

    Add files via upload

---
+++ day2_2ndlargestinarr.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+	int n;
+	printf("Enter the size of the array : ");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter the elements one by one (press enter)");
+	for(int i = 0; i < n; i++){
+		scanf("%d",&arr[i]);
+	}
+	int max_1 = arr[0];
+	int max_2 = arr[0];
+	
+	for(int i=0;i < n;i++){
+		if(arr[i] > max_1){
+			max_2 = max_1;
+			max_1 = arr[i];
+		}else if(arr[i]>max_2 && arr[i]!=max_1){
+			max_2 = arr[i];
+		}
+	}
+	printf("The second largest number in the array is %d",max_2);
+	
+}
